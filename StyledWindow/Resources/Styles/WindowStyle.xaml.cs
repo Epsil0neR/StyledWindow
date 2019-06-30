2fef3717026ff514a3959954ee722e25de12f71e
Epsil0neR <epsil0neris@gmail.com>
2019-06-30 20:53:04 +0300

    Initial commit.

---
+++ StyledWindow/Resources/Styles/WindowStyle.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace StyledWindow.Resources.Styles
+{
+    public partial class WindowStyle
+    {
+        private void EventSetter_OnHandler(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is System.Windows.Window w))
+                return;
+
+
+            w.CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            w.CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            w.CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+            w.CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
+        }
+
+        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = sender is System.Windows.Window w &&
+                           (w.ResizeMode == ResizeMode.CanResize ||
+                            w.ResizeMode == ResizeMode.CanResizeWithGrip);
+        }
+
+        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = sender is System.Windows.Window w && w.ResizeMode != ResizeMode.NoResize;
+        }
+
+        private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            if (target is System.Windows.Window w)
+                SystemCommands.CloseWindow(w);
+        }
+
+        private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            if (target is System.Windows.Window w)
+                SystemCommands.MaximizeWindow(w);
+        }
+
+        private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            if (target is System.Windows.Window w)
+                SystemCommands.MinimizeWindow(w);
+        }
+
+        private void OnRestoreWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            if (target is System.Windows.Window w)
+                SystemCommands.RestoreWindow(w);
+        }
+    }
+}
---
+++ StyledWindow/WindowStyle.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StyledWindow
+{
+    public class WindowStyle
+    {
+        public static readonly DependencyProperty LeftContentProperty = DependencyProperty.RegisterAttached(
+            "LeftContent",
+            typeof(object),
+            typeof(WindowStyle),
+            new FrameworkPropertyMetadata(null,
+                FrameworkPropertyMetadataOptions.AffectsArrange |
+                FrameworkPropertyMetadataOptions.AffectsMeasure)
+        );
+
+        public static void SetLeftContent(UIElement element, object value)
+        {
+            element.SetValue(LeftContentProperty, value);
+        }
+
+        public static object GetLeftContent(UIElement element)
+        {
+            return element.GetValue(LeftContentProperty);
+        }
+
+        public static readonly DependencyProperty MenuProperty = DependencyProperty.RegisterAttached(
+            "Menu",
+            typeof(Menu),
+            typeof(WindowStyle),
+            new FrameworkPropertyMetadata(null,
+                FrameworkPropertyMetadataOptions.AffectsArrange |
+                FrameworkPropertyMetadataOptions.AffectsMeasure)
+        );
+
+        public static void SetMenu(UIElement element, Menu value)
+        {
+            element.SetValue(MenuProperty, value);
+        }
+
+        public static Menu GetMenu(UIElement element)
+        {
+            return (Menu)element.GetValue(MenuProperty);
+        }
+
+        public static readonly DependencyProperty ShowTitleProperty = DependencyProperty.RegisterAttached(
+            "ShowTitle",
+            typeof(bool),
+            typeof(WindowStyle),
+            new FrameworkPropertyMetadata(true,
+                FrameworkPropertyMetadataOptions.AffectsArrange |
+                FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public static void SetShowTitle(UIElement element, bool value)
+        {
+            element.SetValue(ShowTitleProperty, value);
+        }
+
+        public static bool GetShowTitle(UIElement element)
+        {
+            return (bool)element.GetValue(ShowTitleProperty);
+        }
+
+
+        public static readonly DependencyProperty RightContentProperty = DependencyProperty.RegisterAttached(
+            "RightContent",
+            typeof(object),
+            typeof(WindowStyle),
+            new FrameworkPropertyMetadata(null,
+                FrameworkPropertyMetadataOptions.AffectsArrange |
+                FrameworkPropertyMetadataOptions.AffectsMeasure)
+        );
+
+        public static void SetRightContent(UIElement element, object value)
+        {
+            element.SetValue(RightContentProperty, value);
+        }
+
+        public static object GetRightContent(UIElement element)
+        {
+            return element.GetValue(RightContentProperty);
+        }
+    }
+}
